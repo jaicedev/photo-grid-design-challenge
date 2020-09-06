b990af63a1545c6906b9b5440f8f7324f7cbda99
jaicedev <jaicedev@gmail.com>
2020-09-06 00:14:03 -0400

    Minor Changes to Index Page

---
+++ build/precache-manifest.4979e698de16a2e98d13a8183acc0768.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a06e4af8003f86b350c75bb9aacbfc2",
+    "url": "/photo-grid-design-challenge/index.html"
+  },
+  {
+    "revision": "c00f5431bb26dece37c0",
+    "url": "/photo-grid-design-challenge/static/js/2.9e5e4a2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/photo-grid-design-challenge/static/js/2.9e5e4a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042e069c502b4d478311",
+    "url": "/photo-grid-design-challenge/static/js/main.a81f6f4a.chunk.js"
+  },
+  {
+    "revision": "0151d72fa94b15b6365a",
+    "url": "/photo-grid-design-challenge/static/js/runtime-main.f01f0e0e.js"
+  }
+]);
